f453dcff4936fee07550c706c7878984e3ff87e0
Mahesh Sharma <mahesh@fb.com>
2020-01-12 01:48:57 -0800

    Added template

---
+++ utils/template.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+ 
+using namespace std;
+// using namespace __gnu_pbds;
+ 
+#define ford(i, a, b, c)        for(int i=(a); i<(b); i+=(c))
+#define fori(i, a, b)           ford(i,a,b,1)
+#define rep(i, n)               fori(i,0,n)
+#define ifor(i, a, b)           for(int i=(a); i>=(b); i--)
+#define si(x)                   ((int)x.size())
+#define SS                      ({int x;scanf("%d",&x);x;})
+#define pb                      push_back
+#define mp                      make_pair
+#define all(a)                  a.begin(),a.end()
+#define inf                     (int)1e9
+#define linf                    (long long)1e18
+#define V                       vector
+#define S                       string
+#define XX                      first
+#define YY                      second
+#define PVI(v)                  rep(i, si(v)) cout<<v[i]<<" "; puts("")
+#define PPI(p)                  cout<<"("<<p.first<<", "<<p.second<<")\n"
+#define PVPI(v)                 rep(i, si(v)) cout<<"("<<v[i].first<<", "<<v[i].second<<")"<<(i==si(v)-1?"":", ");puts("")
+#define PA(arr1, arr2)          for(auto it = arr1; it != arr2; it++) cout<<*it<<" "; puts("");
+
+typedef V<int> vi;
+typedef V<S> vs;
+typedef long long ll;
+typedef pair<int,int> pii;
+
+/* ################### Program Body starts here ################ */
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    return 0;
+}
+
